0cd320e47c8a53d02f1f6081b6ceb0c2a17dc8c3
ravigajul <ravi.gajul@yahoo.com>
2019-11-13 14:56:26 +0530

    defect fixes 11/13/2019

--- SpecFiles/CSSContainingText.js
+++ SpecFiles/CSSContainingText.js
@@ -1,6 +1,7 @@
 //CSSContainingText Locator
 describe('CSS Containing Text', function() {
   it('CSS containing Text', function() {
+	browser.waitForAngularEnabled(true);
     browser.get('https://qaclickacademy.github.io/protocommerce/');
     element(by.name('name')).sendKeys("Ravi");
     element(by.name('email')).sendKeys("Ravi@test.com");
--- SpecFiles/DropDown.js
+++ SpecFiles/DropDown.js
@@ -3,6 +3,7 @@ describe('Drop Down', function() {
 	
 
   it('should add one and two', function() {
+	browser.waitForAngularEnabled(true);
     browser.get('http://juliemr.github.io/protractor-demo/');
     element(by.model('first')).sendKeys(5);
     element(by.model('second')).sendKeys(2);
