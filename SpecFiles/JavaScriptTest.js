91e148907da3c374f077835f7f30c8927dccec56
ravigajul <rgajul@deloitte.com>
2021-07-19 16:54:51 +0530

    Added java script executor method for clicking and entering data by browser.executeScript

--- SpecFiles/JavaScriptTest.js
+++ SpecFiles/JavaScriptTest.js
@@ -1,6 +1,6 @@
 const { browser, element, by } = require("protractor");
 
-describe('angularjs homepage todo list', function () {
+/* describe('TestCase To check element data entry through java script', function () {
   it('should add a todo', function () {
     browser.get('https://qaclickacademy.github.io/protocommerce/').then(function () {
     var el="document.querySelector(\"div[class='form-group'] input[name='name']\")";
@@ -8,6 +8,21 @@ describe('angularjs homepage todo list', function () {
          browser.sleep(5000);
      });
     console.log("test");
+  });
+  });
+}); */
+
+describe('TestCase To check element data entry through java script another flavour', function () {
+  it('should add a todo', function () {
+    browser.get('https://qaclickacademy.github.io/protocommerce/').then(function () {
+    var el=browser.executeScript("return document.querySelector(\"div[class='form-group'] input[name='name']\")").then(function(el){
+      browser.sleep(3000);
+      return el;
+    });
+    browser.executeScript("arguments[0].value=\"This is spartan\";",el).then(function(){
+      browser.sleep(3000);
+    });
+  console.log("test"); 
   });
   });
 });
